a4e3b4ad1f7423b9363c62c947cb4f338cabdb62
Xe <3312902243@qq.com>
2024-05-09 23:18:30 +0800

    go反向代理

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-re-proxy-http
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+type Proxy struct { //定义了一个名为Proxy的空结构体，它将用作http.Handler的实现
+}
+
+// ServeHTTP方法是http.Handler接口的一部分,实现ListenAndServe()第二个参数里适配器的接口，作为适配器Proxy{}传入
+func (Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	remote, _ := url.Parse("http://127.0.0.1:8082")
+	reverseProxy := httputil.NewSingleHostReverseProxy(remote)
+	reverseProxy.ServeHTTP(w, r)
+}
+
+func main() {
+	addr := "127.0.0.1:8081"
+
+	fmt.Printf("反向代理正在监听：%s\n", addr)
+	err := http.ListenAndServe(addr, Proxy{})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
